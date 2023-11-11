7502459eb9eeb4704fa4f532e33d51333bd1f739
gundorit <packdev937@gmail.com>
2023-11-12 02:12:37 +0900

    feat (OutputView) : 총 혜택 금액을 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
     public static final String EVENT_ANNOUNCEMENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     public static final String TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
     public static final String EXPECTED_AMOUNT_AFTER_DISCOUNT_MESSAGE = "\n<할인 후 예상 결제 금액>";
+    public static final String TOTAL_BENEFIT_MESSAGE = "\n<총혜택 금액>";
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     public static final String NONE_MESSAGE = "없음";
@@ -31,6 +32,11 @@ public class OutputView {
     public static void printExpectedAmountAfterDiscount(int finalAmount) {
         System.out.println(EXPECTED_AMOUNT_AFTER_DISCOUNT_MESSAGE);
         System.out.println(formatter.format(finalAmount));
+    }
+
+    public static void printTotalBenefits(int totalBenefits) {
+        System.out.println(TOTAL_BENEFIT_MESSAGE);
+        System.out.println(formatter.format(-totalBenefits));
     }
 
     public static void printNone() {
