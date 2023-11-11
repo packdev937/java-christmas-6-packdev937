4cf0f140cc434f1ccbc749fdbf1e7a81c6529a0c
gundorit <packdev937@gmail.com>
2023-11-12 02:10:23 +0900

    feat (OutputView) : 할인 후 예상 결제 금액을 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     public static final String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static final String EVENT_ANNOUNCEMENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     public static final String TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
+    public static final String EXPECTED_AMOUNT_AFTER_DISCOUNT_MESSAGE = "\n<할인 후 예상 결제 금액>";
+
     private static final String ERROR_PREFIX = "[ERROR] ";
     public static final String NONE_MESSAGE = "없음";
 
@@ -24,6 +26,11 @@ public class OutputView {
     public static void printTotalAmountBeforeDiscount(int amount) {
         System.out.println(TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE);
         System.out.println(formatter.format(amount));
+    }
+
+    public static void printExpectedAmountAfterDiscount(int finalAmount) {
+        System.out.println(EXPECTED_AMOUNT_AFTER_DISCOUNT_MESSAGE);
+        System.out.println(formatter.format(finalAmount));
     }
 
     public static void printNone() {
