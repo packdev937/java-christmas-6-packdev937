f2a63cb4897db6c80803ece5620c6e8d9f97ba05
gundorit <packdev937@gmail.com>
2023-11-12 01:49:00 +0900

    feat (OutputView) : 이벤트 혜택을 안내하는 문구를 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,9 +3,16 @@ package christmas.view;
 public class OutputView {
 
     public static final String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String EVENT_ANNOUNCEMENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+
 
     public static void printEventStartMessage() {
         System.out.println(EVENT_START_MESSAGE);
     }
+
+    public static void printEventAnnouncement(int visitDate) {
+        System.out.printf(EVENT_ANNOUNCEMENT_MESSAGE, visitDate);
+    }
+
 
 }
