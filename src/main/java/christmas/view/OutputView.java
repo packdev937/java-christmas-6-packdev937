24fad4b21e7037b3420fd2d42eee86cb63b9951e
gundorit <packdev937@gmail.com>
2023-11-12 02:06:50 +0900

    feat (OutputView) : 에러 메세지에는 `[ERROR]`를 함께 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,6 +7,7 @@ public class OutputView {
     public static final String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static final String EVENT_ANNOUNCEMENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     public static final String TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
+    private static final String ERROR_PREFIX = "[ERROR] ";
     private static final DecimalFormat formatter = new DecimalFormat("#,###원");
 
 
@@ -21,5 +22,9 @@ public class OutputView {
     public static void printTotalAmountBeforeDiscount(int amount) {
         System.out.println(TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE);
         System.out.println(formatter.format(amount));
+    }
+
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println(ERROR_PREFIX + errorMessage);
     }
 }
