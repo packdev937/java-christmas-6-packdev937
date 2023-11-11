7ba2265cb765c6890425381eca3da1bc95d6753f
gundorit <packdev937@gmail.com>
2023-11-12 01:46:42 +0900

    feat (OutputView) : 이벤트 플래너 시작 문구를 출력한다

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+public class OutputView {
+
+    public static final String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public static void printEventStartMessage() {
+        System.out.println(EVENT_START_MESSAGE);
+    }
+
+}
