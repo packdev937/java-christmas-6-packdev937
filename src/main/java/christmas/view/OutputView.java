11b4e899696d2ab72df0f2571d79d32ddfdcefc0
gundorit <packdev937@gmail.com>
2023-11-12 02:03:56 +0900

    feat (OutputView) : 할인 전 총 주문 금액을 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,13 @@
 package christmas.view;
 
+import java.text.DecimalFormat;
+
 public class OutputView {
 
     public static final String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static final String EVENT_ANNOUNCEMENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    public static final String TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
+    private static final DecimalFormat formatter = new DecimalFormat("#,###원");
 
 
     public static void printEventStartMessage() {
@@ -14,5 +18,8 @@ public class OutputView {
         System.out.printf(EVENT_ANNOUNCEMENT_MESSAGE, visitDate);
     }
 
-
+    public static void printTotalAmountBeforeDiscount(int amount) {
+        System.out.println(TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE);
+        System.out.println(formatter.format(amount));
+    }
 }
