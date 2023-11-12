b2d54009309d9749b19d85fbf6c3e11c4f8c0608
gundorit <packdev937@gmail.com>
2023-11-12 21:28:25 +0900

    feat (Benefits) : 할인 정책의 리스트의 일급 컬렉션인 Benefits를 생성한다

---
+++ src/main/java/christmas/domain/event/Benefits.java
@@ -0,0 +1,26 @@
+package christmas.domain.event;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Benefits {
+
+    private final List<Benefit> benefits;
+
+    private Benefits() {
+        this.benefits = new ArrayList<>();
+    }
+
+    public static Benefits getInstance() {
+        return new Benefits();
+    }
+
+    public void addBenefit(Benefit benefit) {
+        benefits.add(benefit);
+    }
+
+    public List<Benefit> getBenefits() {
+        return Collections.unmodifiableList(benefits);
+    }
+}
