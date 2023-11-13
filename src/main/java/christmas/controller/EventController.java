311b9fa635c506eef30b470ac63beb7ddab61170
gundorit <packdev937@gmail.com>
2023-11-13 12:42:29 +0900

    feat (EventController) : 입려과 함께 이벤트를 시작한다

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,20 @@
+package christmas.controller;
+
+import christmas.domain.event.VisitDate;
+import christmas.domain.order.OrderItems;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventController {
+
+
+    public EventController() {
+    }
+
+    public void run() {
+        OutputView.printEventStartMessage();
+        VisitDate visitDate = InputView.readVisitDate();
+        OrderItems orderItems = InputView.readOrderMenu();
+        OutputView.printEventAnnouncement(visitDate.getDay());
+    }
+}
