5a63d383429f20a946890a2fbd59511e01492337
gundorit <packdev937@gmail.com>
2023-11-14 20:55:08 +0900

    test (WeekdayDiscountPolicy) : WeekdayDiscountPolicy를 테스트 한다
    - 할인 정책의 이름이 정상적으로 출력되는지 확인한다
    - 평일에만 혜택이 적용되는지 확인한다
    - 디저트 메뉴만 할인 혜택이 적용되는지 확인한다

---
+++ src/test/java/christmas/domain/discount/WeekdayDiscountPolicyTest.java
@@ -0,0 +1,83 @@
+package christmas.domain.discount;
+
+import christmas.domain.event.VisitDate;
+import christmas.domain.order.OrderItems;
+import christmas.domain.menu.MenuItem;
+import christmas.domain.menu.MenuType;
+import christmas.utils.ConstantUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static christmas.utils.ConstantUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("WeekdayDiscountPolicy 클래스")
+public class WeekdayDiscountPolicyTest {
+
+    private final int DISCOUNT_CRITERION = 2023;
+    private WeekdayDiscountPolicy weekdayDiscountPolicy;
+
+    @BeforeEach
+    public void setUp() {
+        weekdayDiscountPolicy = new WeekdayDiscountPolicy();
+    }
+
+    @Test
+    void 할인_정책의_이름을_출력한다() {
+        String description = weekdayDiscountPolicy.getDescription();
+        assertEquals("평일 할인", description);
+    }
+
+    @Test
+    void 평일에는_할인_혜택을_적용한다() {
+        VisitDate weekday = VisitDate.from(LocalDate.of(EVENT_YEAR, EVENT_MONTH, 3));
+        DiscountContext weekdayContext = DiscountContext.of(weekday, null);
+
+        assertTrue(weekdayDiscountPolicy.isApplicable(weekdayContext));
+    }
+
+    @Test
+    void 주말에는_할인_혜택을_적용하지_않는다() {
+        VisitDate weekend = VisitDate.from(LocalDate.of(EVENT_YEAR, EVENT_MONTH, 1));
+        DiscountContext weekendContext = DiscountContext.of(weekend, null);
+
+        assertFalse(weekdayDiscountPolicy.isApplicable(weekendContext));
+    }
+
+    @Test
+    void 평일에는_디저트_개수만큼_할인_혜택을_적용한다() {
+        VisitDate weekdayVisitDate = VisitDate.from(
+            LocalDate.of(EVENT_YEAR, EVENT_MONTH, 3));
+
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.ICE_CREAM, 2);
+        order.put(MenuItem.CHOCOLATE_CAKE, 1);
+        OrderItems orderItems = OrderItems.from(order);
+
+        DiscountContext context = DiscountContext.of(weekdayVisitDate, orderItems);
+
+        assertEquals(order.size() * DISCOUNT_CRITERION,
+            weekdayDiscountPolicy.calculateDiscount(context));
+    }
+
+    @Test
+    void 디저트_이외의_메뉴는_할인_혜택이_적용되지_않는다() {
+        VisitDate weekdayVisitDate = VisitDate.from(
+            LocalDate.of(EVENT_YEAR, EVENT_MONTH, 3));
+
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.T_BONE_STEAK, 2);
+        order.put(MenuItem.RED_WINE, 1);
+        OrderItems orderItems = OrderItems.from(order);
+
+        DiscountContext context = DiscountContext.of(weekdayVisitDate, orderItems);
+
+        assertEquals(0,
+            weekdayDiscountPolicy.calculateDiscount(context));
+    }
+}
