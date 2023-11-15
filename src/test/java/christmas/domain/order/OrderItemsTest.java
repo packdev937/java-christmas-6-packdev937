507b522eb397745ee4532660b5bba5e1edee2d07
gundorit <packdev937@gmail.com>
2023-11-15 14:01:03 +0900

    test (OrderItems) : OrderItems를 테스트 한다
    - MenuType에 따라 MenuItem을 가져온다
    - 해당하는 MenuType이 없다면 빈 값을 반환한다
    - 반환받은 OrderItems은 항상 동일하다
    - toResponse를 호출하면 올바를 OrderItemsResponse를 반환한다
    - 주문에 프로모션이 있다면 True, 없다면 False를 반환한다
    - 음료수만 주문하면 예외를 발생한다
    - 주문을 하지 않으면 예외를 발생한다
    _ 주문이 20개가 넘는다면 예외를 발생한다

--- src/test/java/christmas/domain/order/OrderItemsTest.java
+++ src/test/java/christmas/domain/order/OrderItemsTest.java
@@ -1,11 +1,10 @@
 package christmas.domain.order;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.domain.menu.MenuItem;
 import christmas.domain.menu.MenuType;
+import christmas.dto.OrderItemsResponse;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,16 +17,16 @@ public class OrderItemsTest {
     private OrderItems orderItems;
 
     @BeforeEach
-    public void setUp() {
-        Map<String, Integer> order = new HashMap<>();
-        order.put("바비큐립", 2);
-        order.put("티본스테이크", 1);
-        order.put("아이스크림", 3);
+    void setUp() {
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.BBQ_RIBS, 2);
+        order.put(MenuItem.T_BONE_STEAK, 1);
+        order.put(MenuItem.ICE_CREAM, 3);
         orderItems = OrderItems.from(order);
     }
 
     @Test
-    public void MenuType에_따라_MenuItem을_가져온다() {
+    void MenuType에_따라_MenuItem을_가져온다() {
         Map<MenuItem, Integer> result = orderItems.getItemsByType(MenuType.MAIN);
         assertEquals(2, result.size());
         assertTrue(result.containsKey(MenuItem.BBQ_RIBS));
@@ -35,18 +34,71 @@ public class OrderItemsTest {
     }
 
     @Test
-    public void 해당하는_MenuType이_없다면_빈_값이다() {
+    void 해당하는_MenuType이_없다면_빈_값을_반환한답() {
         Map<MenuItem, Integer> result = orderItems.getItemsByType(MenuType.APPETIZER);
         assertTrue(result.isEmpty());
     }
 
     @Test
-    public void 반환받은_OrderItmes은_항상_동일하다() {
+    void 반환받은_OrderItems는_항상_동일하다() {
         Map<MenuItem, Integer> newOrderItems = orderItems.getOrderItems();
 
         assertNotNull(newOrderItems);
         assertEquals(3, newOrderItems.size());
         assertTrue(newOrderItems.containsKey(MenuItem.BBQ_RIBS));
         assertTrue(newOrderItems.containsKey(MenuItem.T_BONE_STEAK));
+    }
+
+    @Test
+    void toResponse를_호출하면_올바른_OrderItemsResponse를_반환한다() {
+        OrderItemsResponse response = orderItems.toResponse();
+
+        assertNotNull(response);
+        assertEquals(orderItems.getOrderItems(), response.orderItems());
+    }
+
+    @Test
+    void 주문에_프로모션이_포함되어_있을_때_true를_반환한다() {
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.BBQ_RIBS, 3);
+        order.put(MenuItem.CHAMPAGNE, 1);
+        OrderItems orderItemsWithPromotion = OrderItems.from(order);
+
+        assertTrue(orderItemsWithPromotion.containPromotion());
+    }
+
+    @Test
+    void 주문에_프로모션이_포함되어_있지_않을_때_false를_반환한다() {
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.BBQ_RIBS, 3);
+        OrderItems orderItemsWithoutPromotion = OrderItems.from(order);
+
+        assertFalse(orderItemsWithoutPromotion.containPromotion());
+    }
+
+    @Test
+    void 음료수만_주문할_때_예외를_발생한다() {
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.CHAMPAGNE, 5);
+        order.put(MenuItem.RED_WINE, 5);
+
+        assertThrows(IllegalArgumentException.class, () -> OrderItems.from(order));
+    }
+
+    @Test
+    void 아무것도_주문하지_않는다면_예외를_발생한다() {
+        Map<MenuItem, Integer> order = new HashMap<>();
+
+        assertThrows(IllegalArgumentException.class, () -> OrderItems.from(order));
+    }
+
+    @Test
+    void 메뉴를_20개_이상_주문하면_예외를_발생한다() {
+        Map<MenuItem, Integer> order = new HashMap<>();
+        order.put(MenuItem.BBQ_RIBS, 8);
+        order.put(MenuItem.CHAMPAGNE, 8);
+        order.put(MenuItem.RED_WINE, 7);
+
+        assertThrows(IllegalArgumentException.class, () -> OrderItems.from(order));
     }
 }
