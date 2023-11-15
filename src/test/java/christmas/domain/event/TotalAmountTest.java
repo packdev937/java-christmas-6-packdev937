b708db6d0c8f932543b8133861cc93926714450b
gundorit <packdev937@gmail.com>
2023-11-15 14:27:35 +0900

    test (TotalAmount) : TotalAmount를 테스트 한다
    - @ParameterizedTest를 활용해 다양한 경우의 총 금액을 확인한다

--- src/test/java/christmas/domain/event/TotalAmountTest.java
+++ src/test/java/christmas/domain/event/TotalAmountTest.java
@@ -1,37 +1,25 @@
 package christmas.domain.event;
 
-import static christmas.domain.menu.MenuItem.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.domain.menu.MenuItem;
-import christmas.domain.order.OrderItems;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TotalAmountTest {
 
-    private TotalAmount totalAmount;
-    private OrderItems orderItems;
-
-    @BeforeEach
-    public void setUp() {
-        Map<String, Integer> order = new HashMap<>();
-        order.put(T_BONE_STEAK.getName(), 2);
-        order.put(CHOCOLATE_CAKE.getName(), 1);
-
-        orderItems = OrderItems.from(order);
-        totalAmount = TotalAmount.from(orderItems);
-    }
-
-    // 나중에 @Parametrized 사용
-    @Test
-    public void 주문한_메뉴들의_총_금액을_구한다() {
-        totalAmount = TotalAmount.from(orderItems);
-        int expectedAmount = T_BONE_STEAK.getPrice() * 2 + CHOCOLATE_CAKE.getPrice();
-
-        assertEquals(expectedAmount, totalAmount.getAmount());
+    @ParameterizedTest
+    @CsvSource({
+        "T_BONE_STEAK, 2, CHOCOLATE_CAKE, 1",
+        "MUSHROOM_SOUP, 3, TAPAS, 4",
+        "BBQ_RIBS, 2, ICE_CREAM, 3"
+    })
+    public void 주문한_메뉴들의_총_금액을_구한다(MenuItem firstItem, int firstQuantity, MenuItem secondItem,
+        int secondQuantity) {
+        int expectedAmount =
+            firstItem.getPrice() * firstQuantity + firstItem.getPrice() * secondQuantity;
+        TotalAmount totalAmount = TotalAmount.from(expectedAmount);
+        assertEquals(expectedAmount, totalAmount.value());
     }
 
 }
