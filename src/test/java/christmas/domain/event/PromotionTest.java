ce2ac60995852f588073c411ae0329b70c9c67cc
gundorit <packdev937@gmail.com>
2023-11-15 14:56:48 +0900

    test (Promotion) : Promotion을 테스트 한다
    - 프로모션 기준에 따라 상품(샴페인)을 반환하는지 확인한다

---
+++ src/test/java/christmas/domain/event/PromotionTest.java
@@ -0,0 +1,47 @@
+package christmas.domain.event;
+
+import christmas.domain.menu.MenuItem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Promotion 클래스")
+public class PromotionTest {
+
+    private Promotion promotion;
+
+    @BeforeEach
+    void setUp() {
+        promotion = Promotion.from(120000);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "130000", "120000"
+    })
+    void 총_금액이_프로모션_기준과_같거나_그_이상일_때_샴페인_프로모션을_반환한다(int totalAmount) {
+        Promotion result = Promotion.from(totalAmount);
+
+        assertThat(result.item()).isEqualTo(MenuItem.CHAMPAGNE);
+    }
+
+    @Test
+    void 총_금액이_프로모션_기준_미만일_때_프로모션_없음을_반환한다() {
+        int totalAmount = 110000;
+        Promotion result = Promotion.from(totalAmount);
+
+        assertThat(result.item()).isEqualTo(MenuItem.NONE);
+    }
+
+
+    @Test
+    void item_메소드는_프로모션_아이템을_반환한다() {
+        MenuItem result = promotion.item();
+
+        assertThat(result).isEqualTo(MenuItem.CHAMPAGNE);
+    }
+}
