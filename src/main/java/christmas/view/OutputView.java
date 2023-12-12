811bd1362d55f9f144633fb62df429e25b7d9873
gundorit <packdev937@gmail.com>
2023-12-12 22:08:50 +0900

    feat (Outputview) : 이벤트 시작 문구를 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,94 +1,10 @@
 package christmas.view;
 
-import christmas.domain.event.EventBadge;
-import christmas.domain.menu.MenuItem;
-import christmas.dto.BenefitsResponse;
-import christmas.dto.OrderItemsResponse;
-import java.text.DecimalFormat;
+import christmas.utils.messages.OutputMessage;
 
 public class OutputView {
 
-    public static final String EVENT_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    public static final String EVENT_ANNOUNCEMENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
-    public static final String ORDER_MENU_MESSAGE = "\n<주문 메뉴>";
-    public static final String PROMOTION_MESSAGE = "\n<증정 메뉴>";
-    public static final String TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE = "\n<할인 전 총주문 금액>";
-    public static final String BENEFIT_LIST_MESSAGE = "\n<혜택 내역>";
-    public static final String EXPECTED_AMOUNT_AFTER_DISCOUNT_MESSAGE = "\n<할인 후 예상 결제 금액>";
-    public static final String TOTAL_BENEFIT_MESSAGE = "\n<총혜택 금액>";
-    public static final String EVENT_BADGE_MESSAGE = "\n<12월 이벤트 배지>";
-    private static final String ERROR_PREFIX = "[ERROR] ";
-    public static final String NONE_MESSAGE = "없음";
-
-    private static final DecimalFormat formatter = new DecimalFormat("#,###원");
-
-
-    public static void printEventStartMessage() {
-        System.out.println(EVENT_START_MESSAGE);
-    }
-
-    public static void printEventAnnouncement(int visitDate) {
-        System.out.printf(EVENT_ANNOUNCEMENT_MESSAGE, visitDate);
-    }
-
-    public static void printOrderItems(OrderItemsResponse response) {
-        System.out.println(ORDER_MENU_MESSAGE);
-        response.orderItems().forEach((menuItem, quantity) ->
-            System.out.println(menuItem.getName() + " " + quantity + "개"));
-    }
-
-    public static void printTotalAmountBeforeDiscount(int amount) {
-        System.out.println(TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_MESSAGE);
-        System.out.println(formatter.format(amount));
-    }
-
-    public static void printExpectedAmountAfterDiscount(int finalAmount) {
-        System.out.println(EXPECTED_AMOUNT_AFTER_DISCOUNT_MESSAGE);
-        System.out.println(formatter.format(finalAmount));
-    }
-
-    public static void printPromotionItem(MenuItem item) {
-        System.out.println(PROMOTION_MESSAGE);
-        if (item == MenuItem.CHAMPAGNE) {
-            System.out.println(item.getName() + " 1개");
-            return;
-        }
-        System.out.println(item.getName());
-    }
-
-    public static void printTotalBenefits(int totalBenefits) {
-        System.out.println(TOTAL_BENEFIT_MESSAGE);
-        System.out.println(formatter.format(-totalBenefits));
-    }
-
-    public static void printNone() {
-        System.out.print(NONE_MESSAGE);
-    }
-
-    public static void printBenefits(BenefitsResponse response) {
-        System.out.println(BENEFIT_LIST_MESSAGE);
-        if (response.benefits().isEmpty()) {
-            System.out.println(NONE_MESSAGE);
-            return;
-        }
-        response.benefits().stream().forEach((benefit) ->
-            System.out.println(
-	benefit.getDescription() + ": " + formatter.format(-benefit.getDiscountAmount())));
-    }
-
-    public static void printErrorMessage(String errorMessage) {
-        System.out.println(ERROR_PREFIX + errorMessage);
-    }
-
-    public static void printEventBadge(EventBadge eventBadge) {
-        System.out.println(EVENT_BADGE_MESSAGE);
-        if (eventBadge == EventBadge.NONE) {
-            printNone();
-            return;
-        }
-        System.out.println(eventBadge.getBadgeName());
-    }
-
-    private OutputView() {
+    public void displayEventStartMessage() {
+        System.out.print(OutputMessage.EVENT_START_MESSAGE);
     }
 }
